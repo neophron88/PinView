cc73d05daf632d9f1d616012fd416f4df8ffc33d
Fazliddin <itrf0088@gmail.com>
2024-06-30 14:07:10 +0500

    setup module to publication

--- pinview/build.gradle.kts
+++ pinview/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -37,7 +38,19 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.github.neophron88"
+            artifactId = "PinView"
+            version = "1.0"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
 }
--- sample/src/main/java/com/pulchukur/pinview_sample/MainActivity.kt
+++ sample/src/main/java/com/pulchukur/pinview_sample/MainActivity.kt
@@ -2,12 +2,6 @@ package com.pulchukur.pinview_sample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.postDelayed
-import com.pulchukur.pinview.PinView
-import com.pulchukur.pinview.standard.behaviors.appearance.PinBehaviorAnimatedAppearance
-import com.pulchukur.pinview.standard.behaviors.appearance.animation.PinAnimFromBottomToBottom
-import com.pulchukur.pinview.standard.behaviors.appearance.animation.PinAnimFromBottomToTop
-import com.pulchukur.pinview.standard.behaviors.cursor.PinBehaviorCursor
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
